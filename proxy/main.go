03b9936b51d07614b0cabe53fbf60e4e2c308f43
kaancang <k_goet@outlook.de>
2023-09-27 03:49:25 +0200

    add basic proxy

---
+++ proxy/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func handler(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading request body"})
+		return
+	}
+
+	req, err := http.NewRequest(c.Request.Method, "https://api.openai.com"+c.Request.URL.Path, bytes.NewBuffer(body))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	req.Header.Set("Authorization", c.GetHeader("Authorization"))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), respBody)
+}
+
+func main() {
+	r := gin.Default()
+	r.Any("/*any", handler)
+	r.Run(":3000")
+}

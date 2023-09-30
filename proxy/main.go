67ecdd745c3c2766138fafd4acd715d61c99cf5f
kaancang <k_goet@outlook.de>
2023-09-30 02:45:50 +0200

    add-basic-json-path-handling

--- proxy/main.go
+++ proxy/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"client-go/model"
 	"fmt"
+	"github.com/gin-gonic/gin/binding"
 	"io"
 	"log"
 	"net/http"
@@ -18,7 +20,23 @@ func handler(c *gin.Context) {
 		return
 	}
 
+	// You need to create a new ReadCloser for the request body, because it was exhausted by io.ReadAll
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	var chatRequest model.CreateChatCompletionRequest
+	err = c.ShouldBindBodyWith(&chatRequest, binding.JSON)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error binding request body"})
+		return
+	}
 	openAiRequestUri := c.Request.RequestURI
+
+	fmt.Println("openAiRequestUri", openAiRequestUri)
+
+	if err != nil {
+		fmt.Println(http.StatusInternalServerError, gin.H{"error": "Error reading request body"})
+	}
+
 	req, err := http.NewRequest(c.Request.Method, "https://api.openai.com/v1"+c.Request.URL.Path, bytes.NewBuffer(body))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
